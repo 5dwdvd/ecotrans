1d1a2a698b2f202cd8095c5f7783f0953afda760
David <wudw.david@gmail.com>
2022-10-02 08:19:06 +0100

    added stations

--- qr-backend/qr_backend/generate.py
+++ qr-backend/qr_backend/generate.py
@@ -3,16 +3,19 @@ from datetime import datetime
 import hashlib
 
 current_30_timestamp = f"{datetime.now().timestamp()//30:.0f}"
-hashed_string = hashlib.sha256(current_30_timestamp.encode('utf-8')).hexdigest()
+hashed_string = hashlib.sha256(current_30_timestamp\
+    .encode('utf-8')).hexdigest()
 
-def qr_now(name):
+def qr_now(name,location):
     qr = qrcode.QRCode(
         version=7,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=100,
         border=4,
     )
-    qr.add_data(hashed_string)
+    qr.add_data(f"{location}-{hashed_string}")
     qr.make(fit=True)
-    img = qr.make_image(fill_color=(89, 173, 148), back_color=(255, 254, 249))
+    img = qr.make_image(fill_color=(89, 173, 148),
+                        back_color=(255, 254, 249))
     img.save(name)
+
